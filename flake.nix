34af6529d5e86a0dd4ba5ddae21c9648dea4887e
osbm <osbm@osbm.dev>
2026-02-27 10:31:59 +0300

    init flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  description = "PayNKolay API Spec";
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+  outputs = { self, nixpkgs }:
+    let
+      forAllSystems = nixpkgs.lib.genAttrs [
+        "x86_64-linux"
+        "aarch64-linux"
+        "x86_64-darwin"
+        "aarch64-darwin"
+      ];
+      makePkgs = system: import nixpkgs { inherit system; };
+    in
+    {
+    devShells = forAllSystems (system: let
+      pkgs = makePkgs system;
+    in {
+      default = pkgs.mkShell {
+        buildInputs = [
+          pkgs.openapi-generator-cli
+          pkgs.nodejs
+          pkgs.redocly-cli
+        ];
+      };
+    });
+  };
+}
